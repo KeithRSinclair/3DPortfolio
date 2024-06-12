7e7ebbe92f6b1d3368f33a868fef8fc8228bb90e
KeithRSinclair <keithr.sinclair@gmail.com>
2024-06-12 13:11:35 +0700

    Not Camel Case Clown

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter baseName="/3DPortfolio">
+    <BrowserRouter basename="/3DPortfolio">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
