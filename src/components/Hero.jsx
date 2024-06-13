60031fc6bd4bea54304f9b758e87eabbd7a8ded7
KeithRSinclair <keithr.sinclair@gmail.com>
2024-06-13 17:04:26 +0700

    some more small changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,23 @@ import { ComputersCanvas } from "./canvas";
 import { styles } from "../styles";
 
 const Hero = () => {
-  return <section className="relative w-full h-screen mx-auto"></section>;
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xlmx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi I'm <span className="text-[#915eff]">Keith</span>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
